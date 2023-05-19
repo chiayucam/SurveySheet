b5b57cc271ef64ab46710d2c7a2a270e9f2de2a0
chiayucam <48814609+chiayucam@users.noreply.github.com>
2023-05-19 23:11:01 +0800

    feat: Removed default controller and added SheetController

---
+++ SurveySheet/Controllers/SheetController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SurveySheet.Services;
+
+namespace SurveySheet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SheetController : ControllerBase
+    {
+        private ISheetService SheetService;
+
+        public SheetController(ISheetService sheetService)
+        {
+            SheetService = sheetService;
+        }
+
+        
+    }
+}
